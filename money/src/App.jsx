11aaa3e1fccd28b912c5d32f1d9896a073d67e5b
INEEDMONEY$ <121662005+INEEDDMONEYY@users.noreply.github.com>
2025-09-13 00:14:45 +0000

    Base path is broken for vercel need to find a fix.

--- money/src/App.jsx
+++ money/src/App.jsx
@@ -10,14 +10,14 @@ function App() {
     <>
       <main>
         <div className="app-bg">
-          <div className="m-10 flex justify-between">
-            <div className="absolute left-70 rounded-lg w-[700px]">
+          <div className="m-10 flex justify-evenly app-container">
+            <div className=" rounded-lg w-[700px] flex flex-col items-center">
               <img src={Logo} alt="Company logo" className="lp-logo" />
-              <div className="absolute bottom-10 rounded-md app-content">
+              <div className="rounded-md app-content">
                 <h1 className="text-center text-[2rem]">Is your vacation too short because of how expensive it is? Here at Traverse, find the best deals on hotels & flights !</h1>
               </div>
             </div>
-            <div className="text-center absolute top-200 right-70">
+            <div className="text-center absolute top-150 ">
               <button className="text-[1.4rem] rounded-md lp-btn">Travel with us <Link to="/booking"><ArrowRightFromLine className="inline-block ml-2 mb-1 lp-btn-icon" /></Link></button>
             </div>
           </div>
